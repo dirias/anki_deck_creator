f69d7c1aeedf01d5bcb1c33effdc45c2f99e0492
Didier Irias <didier@firstfactory.com>
2023-05-21 12:42:00 -0600

    Updating config variables

--- create_deck.py
+++ create_deck.py
@@ -1,11 +1,11 @@
 from License_driver_CR.data_license_driver import generate_cap_2
 import genanki
 
-# Parameters
+# Config variables
 DECK_NAME = 'Capitulo 2'
 APKG_NAME = 'cap2.apkg'
+DATA_SOURCE = generate_cap_2()
 
-data_driver_license = generate_cap_2()
 # Define Anki note model
 model_id = 1607392319
 model = genanki.Model(
@@ -31,7 +31,7 @@ model = genanki.Model(
 # Generate Anki cards and add them to a deck
 deck_id = 2059400110
 deck = genanki.Deck(deck_id, DECK_NAME)
-for note in data_driver_license.values():
+for note in DATA_SOURCE.values():
     for dir_name, description in note:
         note = genanki.Note(model=model, fields=[dir_name, description])
         deck.add_note(note)
