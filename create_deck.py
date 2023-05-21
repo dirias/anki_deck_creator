c0f748e0682d615da484527bbf4bfa9e29b730ab
Didier Irias <didier@firstfactory.com>
2023-05-21 12:39:42 -0600

    Adding configuration variables

--- create_deck.py
+++ create_deck.py
@@ -1,6 +1,10 @@
 from License_driver_CR.data_license_driver import generate_cap_2
 import genanki
 
+# Parameters
+DECK_NAME = 'Capitulo 2'
+APKG_NAME = 'cap2.apkg'
+
 data_driver_license = generate_cap_2()
 # Define Anki note model
 model_id = 1607392319
@@ -26,12 +30,11 @@ model = genanki.Model(
 
 # Generate Anki cards and add them to a deck
 deck_id = 2059400110
-deck = genanki.Deck(deck_id, 'Capitulo 2')
-import pdb; pdb.set_trace()
+deck = genanki.Deck(deck_id, DECK_NAME)
 for note in data_driver_license.values():
     for dir_name, description in note:
         note = genanki.Note(model=model, fields=[dir_name, description])
         deck.add_note(note)
 
 # Save the deck to an Anki package (*.apkg) file
-genanki.Package(deck).write_to_file('cap2.apkg')
+genanki.Package(deck).write_to_file(APKG_NAME)
