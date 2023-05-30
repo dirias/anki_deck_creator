ebe6e85321b730c97a30386e15c49d2f1b8f35b4
Didier Irias <didier@firstfactory.com>
2023-05-30 14:55:24 -0600

    Adding AI info

---
+++ Artifitial_Intelligence/AI_data.py
@@ -0,0 +1,49 @@
+redes_neuronales_conceptos = [
+    ("¿Por qué buscar construir una inteligencia artificial?", "Para reproducir algunas de las funciones del ser humano."),
+    ("¿Qué es el aprendizaje de máquina?", "Capacidad de una máquina de aprender un proceso en base a los datos que recibe."),
+    ("¿En qué se basan los primeros modelos?", "Perceptrón simple (neurona única) representada por un modelo matemático."),
+    ("¿Cómo funciona una neurona?", "Tiene entradas, el procesamiento y discriminador, salida."),
+    ("¿Cuál es el funcionamiento básico de un perceptrón?", "Se multiplican los pesos por las entradas, se pasa a través del discriminador y se mete a una función de activación, puede ser la Sigmoide."),
+    ("El aprendizaje de máquina sirve para...", "Responder preguntas y procesar datos."),
+    ("¿Qué son los dendritos?", "Lugares en donde entra la información a la neurona."),
+    ("¿Cuál es la función del núcleo en una neurona?", "Centro de procesamiento de información."),
+    ("¿Qué es el axón en una neurona?", "Conector entre el núcleo y las terminales."),
+    ("¿Qué es la sinapsis en el contexto de las neuronas?", "Conexiones entre neuronas."),
+    ("¿Qué determina si la información se transmite a otras neuronas en función del nivel de activación de la información de entrada?", "El nivel de activación de la información de entrada."),
+    ("¿Qué factores influyen en la intensidad con la que se transmite la información entre neuronas?", "La actividad que esté siguiendo y el módulo de redes neuronales que estén interviniendo."),
+    ("¿Qué son los pesos en una neurona artificial?", "Fuerza de conexión entre las entradas y el núcleo."),
+    ("¿Cuál es la función del bias en una neurona artificial?", "Determinar si la información se transmite a las siguientes neuronas."),
+    ("¿Qué son las conexiones 'out' en una neurona artificial?", "Conexiones entre neuronas."),
+    ("¿Qué imitan las funciones de activación en una neurona artificial?", "Los procesos de aprendizaje de forma más natural.")
+]
+
+redes_neuronales_profundas = [
+    ("¿Qué hacen las redes neuronales profundas?", "Aumentar el número de capas y neuronas para interpretar patrones cada vez más complejos."),
+    ("¿Qué es el Descenso del Gradiente?", "Es un algoritmo que optimiza una red neuronal para minimizar el error."),
+    ("¿Cómo funciona el Descenso del Gradiente?", "Utiliza la optimización de una curva para acercarse progresivamente al punto mínimo."),
+    ("¿Qué es el Backpropagation?", "Es un proceso iterativo que permite encontrar el mínimo de una función."),
+    ("¿En qué consiste el entrenamiento de una red neuronal?", "Consiste en inicializar los pesos con valores aleatorios, obtener el resultado de la evaluación de la red, compararlo con el resultado deseado, calcular el error y ajustar los pesos y el bias."),
+    ("¿Qué se busca encontrar durante el entrenamiento de una red neuronal?", "Los valores de los pesos y el bias que arrojen el mínimo de la función de costo."),
+    ("¿Cuál es el objetivo del Descenso del Gradiente?", "Lograr una total optimización de cada una de las neuronas que componen la red neuronal."),
+    ("¿Qué se repite durante el entrenamiento de una red neuronal?", "La comparación de resultados, el cálculo del error y el ajuste de pesos y bias hasta que se cumpla un criterio de éxito en relación con los datos de entrenamiento."),
+    ("¿Qué es el bias en una neurona artificial?", "Es un discriminador que determina si la información se transmite a las siguientes neuronas."),
+    ("¿Qué son los pesos en una neurona artificial?", "Son la fuerza de conexión entre las entradas y el núcleo de una neurona artificial.")
+
+]
+
+tensorflow = [
+    ("¿Qué es TensorFlow?", "Empezó siendo una librería open source, aunque ahora es un entorno completo de desarrollo."),
+    ("Además de ser una librería, ¿qué ofrece TensorFlow?", "No solo ofrece librerías para programar modelos de inteligencia artificial, sino también herramientas para visualizar, optimizar y compartir nuestros modelos con el resto del mundo."),
+    ("¿Cuáles son los pasos básicos para crear una aplicación sencilla en TensorFlow?", "Tener los datos, diseñar tu modelo, entrenarlo, verificar que el entrenamiento fue correcto y utilizarlo para hacer predicciones.")
+]
+diferencias_tf_tfjs = [
+    ("¿Qué capacidades agrega TF.js a cualquier aplicación web?", "Podrás agregar capacidades de aprendizaje de máquina."),
+    ("¿Dónde puedes crear y entrenar modelos con TF.js?", "Puedes crear y entrenar modelos en el Navegador o en un servidor en Node."),
+    ("¿Qué puedes hacer con TF.js en tu entorno de JavaScript?", "Puedes utilizar modelos pre-entrenados."),
+    ("¿Qué diferencia existe en la recolección de basura entre JavaScript y TensorFlow.js?", "A diferencia del código de JavaScript, los objetos y variables utilizados por TensorFlow.js no se liberan automáticamente de la memoria cuando salen del alcance."),
+    ("¿Qué función se utiliza en TensorFlow.js para limpiar los tensores intermedios?", "Es necesario utilizar la función tf.tidy()."),
+    ("¿Qué funciones existen en TensorFlow.js para medir el desempeño y realizar un perfilado de la ejecución?", "Existen funciones para medir el desempeño y realizar un perfilado de los flujos y modelos."),
+    ("¿Cuáles son algunas de las clases principales en TensorFlow.js?", "Algunas clases principales son: Tensores (Tensors), Modelos (Models), Capas (Layers), Operaciones (Operations), Entrenamiento (Training), Desempeño (Performance), Entorno (Environment), Restricciones (Constraints), Inicializadores (Initializers), Regularizadores (Regularizers), Datos (Data), Visualización (Visualization), Utilidades (Utils), Backends, Navegador (Browser), Métricas (Metrics) y Callbacks.")
+]
+
+
